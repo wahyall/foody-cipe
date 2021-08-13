15cb664d06f504c28da1ac606b51357931be41d2
wahyall <wahyall278@gmail.com>
2021-08-13 09:23:09 +0700

    fix see more issues

--- src/component/SeeMore/SeeMore.jsx
+++ src/component/SeeMore/SeeMore.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './SeeMore.scss';
 import { Link } from 'react-router-dom';
 
@@ -10,6 +10,8 @@ import arrow from '../../icon/arrow-black.svg';
 import search from '../../icon/search.svg';
 
 const SeeMore = (props) => {  
+  const content = useRef(null);
+
   const recipeCards = props.data.map(recipe => (
     <JumboCard.Full
       key={recipe.id}
@@ -34,15 +36,21 @@ const SeeMore = (props) => {
     <main className={"see-more" + (props.active ? ' active' : '')}>
       <header>
         <img src={arrow} alt="back" className="back"
-          onClick={() => (
-            props.onCloseSeeMore({ ...props, active: false })
-          )} />
+          onClick={() => {
+            props.onCloseSeeMore({ ...props, active: false });
+
+            // Auto scroll kembali ke atas, agar saat dibuka lagi berada di atas
+            setTimeout(() => {
+              // Menunggu animasi swipe selesai
+              content.current.scrollTop = 0;
+            }, 200);
+          }} />
         <div className="title">{props.title}</div>
         <Link to="/search" className="search">
           <img src={search} alt="search" />
         </Link>
       </header>
-      <section className="content">
+      <section className="content" ref={content}>
         {props.data.length ? recipeCards : loadingCards}
       </section>
     </main>
