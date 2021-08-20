e519de53e402e74834ac38f3d22e236c92505c78
wahyall <wahyall278@gmail.com>
2021-08-20 19:20:59 +0700

    fix cookbook click state

--- src/page/Cookbook/CookbookItem/CookbookItem.jsx
+++ src/page/Cookbook/CookbookItem/CookbookItem.jsx
@@ -17,31 +17,33 @@ const CookbookItem = (props) => {
 
   return (
     <div className="cookbook-item">
-      {props.data.length === 1 ? (
-        // Jika jumlah resep di dalam Cookbook sama dengan 1
-        <div className="thumbnail type-1">
-          <div style={{backgroundImage: `url(${props.data[0].image})`}}></div>
-        </div>
-      ) : null}
-      {props.data.length === 2 ? (
-        // Jika jumlah resep di dalam Cookbook sama dengan 2
-        <div className="thumbnail type-2">
-          <div style={{backgroundImage: `url(${props.data[0].image})`}}></div>
-          <div style={{backgroundImage: `url(${props.data[1].image})`}}></div>
-        </div>
-      ) : null}
-      {props.data.length >= 3 ? (
-        // Jika jumlah resep di dalam Cookbook lebih besar sama dengan 3
-        <div className="thumbnail type-3">
-          <div style={{backgroundImage: `url(${props.data[0].image})`}}></div>
-          <div style={{backgroundImage: `url(${props.data[1].image})`}}></div>
-          <div style={{backgroundImage: `url(${props.data[2].image})`}}></div>
-        </div>
-      ) : null}
-      <Link to={"/cookbook/" + props.id} className="content">
-        <span className="name">{props.name}</span>
-        <span className="amount">{props.data.length} {props.data.length > 1 ? ' Recipes' : 'Recipe'}</span>
-      </Link>
+      <div className="body">
+        {props.data.length === 1 ? (
+          // Jika jumlah resep di dalam Cookbook sama dengan 1
+          <div className="thumbnail type-1">
+            <div style={{backgroundImage: `url(${props.data[0].image})`}}></div>
+          </div>
+        ) : null}
+        {props.data.length === 2 ? (
+          // Jika jumlah resep di dalam Cookbook sama dengan 2
+          <div className="thumbnail type-2">
+            <div style={{backgroundImage: `url(${props.data[0].image})`}}></div>
+            <div style={{backgroundImage: `url(${props.data[1].image})`}}></div>
+          </div>
+        ) : null}
+        {props.data.length >= 3 ? (
+          // Jika jumlah resep di dalam Cookbook lebih besar sama dengan 3
+          <div className="thumbnail type-3">
+            <div style={{backgroundImage: `url(${props.data[0].image})`}}></div>
+            <div style={{backgroundImage: `url(${props.data[1].image})`}}></div>
+            <div style={{backgroundImage: `url(${props.data[2].image})`}}></div>
+          </div>
+        ) : null}
+        <Link to={"/cookbook/" + props.id} className="content">
+          <span className="name">{props.name}</span>
+          <span className="amount">{props.data.length} {props.data.length > 1 ? ' Recipes' : 'Recipe'}</span>
+        </Link>
+      </div>
 
       {/* Jika cookbook-item yang ditampilkan adalah Favorite Recipes */}
       {/* maka jangan tampilkan "option" karena Favorite Recipes tidak boleh diedit atau dihapus */}
