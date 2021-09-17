2a3d6565cfc4471c31119306743c99961024e8a5
wahyall <wahyall278@gmail.com>
2021-09-17 19:17:09 +0700

    create error boundary

---
+++ src/component/ErrorBoundary/ErrorBoundary.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// Page
+import Error from '../../page/Error/Error';
+
+class ErrorBoundary extends React.Component {
+  state = {
+    hasError: false
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <Error />
+    }
+
+    return this.props.children;
+  }
+}
